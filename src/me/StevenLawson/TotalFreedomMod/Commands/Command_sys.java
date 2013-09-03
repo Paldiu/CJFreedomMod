70540eeac242ca2e4204ff720bcbdd5ff97ffc6c
Ryan <ryan.ictclubsales@gmail.com>
2013-09-03 14:14:13 +0100

    Improved security of SYS command, and also started to rework the /sys superdoom system... Its still a work in progress, but is getting there :)

--- src/me/StevenLawson/TotalFreedomMod/Commands/Command_sys.java
+++ src/me/StevenLawson/TotalFreedomMod/Commands/Command_sys.java
@@ -1,8 +1,8 @@
 package me.StevenLawson.TotalFreedomMod.Commands;
 
+import me.StevenLawson.TotalFreedomMod.TFM_ServerInterface;
 import me.StevenLawson.TotalFreedomMod.TFM_Superadmin;
 import me.StevenLawson.TotalFreedomMod.TFM_SuperadminList;
-import me.StevenLawson.TotalFreedomMod.TFM_ServerInterface;
 import me.StevenLawson.TotalFreedomMod.TFM_Util;
 import me.StevenLawson.TotalFreedomMod.TotalFreedomMod;
 import org.bukkit.ChatColor;
@@ -13,7 +13,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.scheduler.BukkitRunnable;
 
 @CommandPermissions(level = AdminLevel.ALL, source = SourceType.BOTH)
-@CommandParameters(description = "System Administration Management", usage = "/<command> <<saadd|sadelete|superdoom> <username>>")
+@CommandParameters(description = "System Administration Management", usage = "/<command> <donatorworld | <saadd|sadelete> <username>>")
 public class Command_sys extends TFM_Command
 {
     @Override
@@ -23,6 +23,7 @@ public class Command_sys extends TFM_Command
         if (!TFM_Util.SYSADMINS.contains(sender.getName()))
         {
             sender.sendMessage(TotalFreedomMod.MSG_NO_PERMS);
+            TFM_Util.adminAction("WARNING: " + sender.getName(), "Has attempted to use a system admin only command. System administration team has been alerted.", true);
             return true;
         }
 
@@ -94,18 +95,13 @@ public class Command_sys extends TFM_Command
 
                 TFM_SuperadminList.removeSuperadmin(target_name);
             }
-            else if (args[0].equalsIgnoreCase("superdoom"))
+
+            if (args[0].equalsIgnoreCase("superdoom"))
             {
-
-                if (args.length != 1)
-                {
-                    return false;
-                }
-
                 final Player player;
                 try
                 {
-                    player = getPlayer(args[0]);
+                    player = getPlayer(args[1]);
                 }
                 catch (PlayerNotFoundException ex)
                 {
@@ -113,29 +109,16 @@ public class Command_sys extends TFM_Command
                     return true;
                 }
 
-                TFM_Util.adminAction(sender.getName(), "Casting a shadow of oblivion over " + player.getName(), true);
-                TFM_Util.bcastMsg(player.getName() + " will be completely obliviated using the SUPER DOOM!", ChatColor.RED);
+                TFM_Util.adminAction(sender.getName(), "Casting a dark shadow of oblivion over " + player.getName(), true);
+                TFM_Util.bcastMsg(player.getName() + " will be completely obliviated!", ChatColor.RED);
 
                 final String IP = player.getAddress().getAddress().getHostAddress().trim();
-
-                // remove from superadmin
-                if (TFM_SuperadminList.isUserSuperadmin(player))
-                {
-                    TFM_Util.adminAction(sender.getName(), "Removing " + player.getName() + " from the superadmin list.", true);
-                    TFM_SuperadminList.removeSuperadmin(player);
-                }
 
                 // remove from whitelist
                 player.setWhitelisted(false);
 
                 // deop
                 player.setOp(false);
-
-                // ban IP
-                TFM_ServerInterface.banIP(IP, null, null, null);
-
-                // ban name
-                TFM_ServerInterface.banUsername(player.getName(), null, null, null);
 
                 // set gamemode to survival
                 player.setGameMode(GameMode.SURVIVAL);
@@ -157,9 +140,6 @@ public class Command_sys extends TFM_Command
                     {
                         // strike lightning
                         player.getWorld().strikeLightning(player.getLocation());
-
-                        // kill (if not done already)
-                        player.setHealth(0.0);
                     }
                 }.runTaskLater(plugin, 20L * 2L);
 
@@ -173,126 +153,20 @@ public class Command_sys extends TFM_Command
                     {
                         // strike lightning
                         player.getWorld().strikeLightning(player.getLocation());
-
-                        // kill (if not done already)
-                        player.setHealth(0.0);
                     }
                 }.runTaskLater(plugin, 20L * 2L);
 
-                // generate explosion
-                player.getWorld().createExplosion(player.getLocation(), 4F);
+                // message
+                TFM_Util.adminAction(player.getName(), "Has been Superdoomed, may the hell continue ", true);
 
-                new BukkitRunnable()
-                {
-                    @Override
-                    public void run()
-                    {
-                        // strike lightning
-                        player.getWorld().strikeLightning(player.getLocation());
+                // ignite player
+                player.setFireTicks(10000);
 
-                        // kill (if not done already)
-                        player.setHealth(0.0);
-                    }
-                }.runTaskLater(plugin, 20L * 2L);
+                // ban IP
+                TFM_ServerInterface.banIP(IP, null, null, null);
 
-                // generate explosion
-                player.getWorld().createExplosion(player.getLocation(), 4F);
-
-                new BukkitRunnable()
-                {
-                    @Override
-                    public void run()
-                    {
-                        // strike lightning
-                        player.getWorld().strikeLightning(player.getLocation());
-
-                        // kill (if not done already)
-                        player.setHealth(0.0);
-                    }
-                }.runTaskLater(plugin, 20L * 2L);
-
-                // generate explosion
-                player.getWorld().createExplosion(player.getLocation(), 4F);
-
-                new BukkitRunnable()
-                {
-                    @Override
-                    public void run()
-                    {
-                        // strike lightning
-                        player.getWorld().strikeLightning(player.getLocation());
-
-                        // kill (if not done already)
-                        player.setHealth(0.0);
-                    }
-                }.runTaskLater(plugin, 20L * 2L);
-
-                // generate explosion
-                player.getWorld().createExplosion(player.getLocation(), 4F);
-
-                new BukkitRunnable()
-                {
-                    @Override
-                    public void run()
-                    {
-                        // strike lightning
-                        player.getWorld().strikeLightning(player.getLocation());
-
-                        // kill (if not done already)
-                        player.setHealth(0.0);
-                    }
-                }.runTaskLater(plugin, 20L * 2L);
-
-                // generate explosion
-                player.getWorld().createExplosion(player.getLocation(), 4F);
-
-                new BukkitRunnable()
-                {
-                    @Override
-                    public void run()
-                    {
-                        // strike lightning
-                        player.getWorld().strikeLightning(player.getLocation());
-
-                        // kill (if not done already)
-                        player.setHealth(0.0);
-                    }
-                }.runTaskLater(plugin, 20L * 2L);
-
-                TFM_Util.adminAction(sender.getName(), "Is continuing to Superdoom " + player.getName(), true);
-
-                // generate explosion
-                player.getWorld().createExplosion(player.getLocation(), 4F);
-
-                new BukkitRunnable()
-                {
-                    @Override
-                    public void run()
-                    {
-                        // strike lightning
-                        player.getWorld().strikeLightning(player.getLocation());
-
-                        // kill (if not done already)
-                        player.setHealth(0.0);
-                    }
-                }.runTaskLater(plugin, 20L * 2L);
-
-                // generate explosion
-                player.getWorld().createExplosion(player.getLocation(), 4F);
-
-                new BukkitRunnable()
-                {
-                    @Override
-                    public void run()
-                    {
-                        // strike lightning
-                        player.getWorld().strikeLightning(player.getLocation());
-
-                        // kill (if not done already)
-                        player.setHealth(0.0);
-                    }
-                }.runTaskLater(plugin, 20L * 2L);
-
+                // ban name
+                TFM_ServerInterface.banUsername(player.getName(), null, null, null);
 
                 new BukkitRunnable()
                 {
@@ -300,18 +174,20 @@ public class Command_sys extends TFM_Command
                     public void run()
                     {
                         // message
-                        TFM_Util.adminAction(sender.getName(), "Banning " + player.getName() + ", IP: " + IP, true);
+                        TFM_Util.adminAction(sender.getName(), "Has Superdoomed: " + player.getName() + ", IP: " + IP, true);
 
                         // generate explosion
                         player.getWorld().createExplosion(player.getLocation(), 4F);
 
                         // kick player
-                        player.kickPlayer(ChatColor.RED + "FUCKOFF, You just got super doomed!");
+                        player.kickPlayer(ChatColor.RED + "FUCKOFF, and get your shit together you super doomed cunt!");
                     }
                 }.runTaskLater(plugin, 20L * 3L);
+
             }
-        else
+            else
             {
+
                 return false;
             }
 
